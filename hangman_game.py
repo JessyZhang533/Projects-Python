46c4c3ac92f6c4c85696426a10edffc2d31f8619
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-11 22:44:27 +0100

    hangman v3

--- hangman_game.py
+++ hangman_game.py
@@ -4,6 +4,11 @@
 # 3..split(): split a string into a list where each word is an item; default separator: white space
 # 4.Letters in a string can be iterated, can use len() to decide the length, can be indexed into.
 # 5.Difference between i+1 and i+=1: https://stackoverflow.com/questions/41446833/what-is-the-difference-between-i-i-1-and-i-1-in-a-for-loop
+# 6."UnboundLocalError: local variable '...' referenced before assignment":
+# https://stackoverflow.com/questions/10851906/python-3-unboundlocalerror-local-variable-referenced-before-assignment
+# Cannot set the value of a global variable directly inside a function. Need to declare 'global ...' insidethe top of the function
+# 7.'break' statement in a loop within a loop: will only break the inner loop
+
 import random
 
 
@@ -12,9 +17,13 @@ word_list = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyot
 correct_word = word_list[random.randint(0, len(word_list) - 1)]
 print("Correct word: {}".format(correct_word))
 
+# Define global variables
 num_of_guesses = 0
 display_result = "_" * len(correct_word)
 num_of_remaining_letters = len(correct_word)
+
+# Check if the game is done
+game_is_done = False
 
 
 def get_input():
@@ -46,11 +55,30 @@ def process_input_and_display(user_input, num_of_guesses):
     return "Status: {}".format(display_result)
 
 
-# Main body of the game
-while num_of_guesses < 7:
-    user_input, number = get_input()
-    print(user_input)
-    status_now = process_input_and_display(user_input, number)
-    print(status_now)
-    if status_now == "You lose!" or status_now == "You win!":
-        break
+while True:
+    # Main body of the game
+    while num_of_guesses < 7:
+        user_input, number = get_input()
+        print(user_input)
+        status_now = process_input_and_display(user_input, number)
+        print(status_now)
+        if status_now == "You lose!" or status_now == "You win!":
+            game_is_done = True
+            break
+
+    if game_is_done is True:
+        user_choice = input("Do you want to play it again? (enter yes or no)")
+        if user_choice.lower() == 'yes':  # need to initialise again as follows
+            # Generate the word one needs to guess
+            correct_word = word_list[random.randint(0, len(word_list) - 1)]
+            print("Correct word: {}".format(correct_word))
+
+            # Define global variables
+            num_of_guesses = 0
+            display_result = "_" * len(correct_word)
+            num_of_remaining_letters = len(correct_word)
+
+            # Check if the game is done
+            game_is_done = False
+        else:
+            break
