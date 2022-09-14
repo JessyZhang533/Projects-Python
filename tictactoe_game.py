560cbf8e68ecd5d82d9f4a262b271e80ef5e417d
JinxuanZhang <77959343+JessyZhang533@users.noreply.github.com>
2022-09-14 21:49:34 +0100

    tictactoe player input related

--- tictactoe_game.py
+++ tictactoe_game.py
@@ -9,4 +9,22 @@ def display_board():
     print(f"{board[0]} {board[1]} {board[2]}\n{board[3]} {board[4]} {board[5]}\n{board[6]} {board[7]} {board[8]}")
 
 
+def get_input_from_player():
+    " Get input from player(human) "
+    player_input = input("Where to place your next move? (1-9)")
+    return player_input
+
+
+def player_move(player_input, player_letter):
+    " Associate the input from the player to a change on the board "
+    player_input = int(player_input)
+    if player_input < 1 or player_input > 9:
+        player_input = int(get_input_from_player())
+        player_move(player_input, player_letter=player_letter)
+    else:
+        board[player_input - 1] = player_letter
+
+
+player_input = get_input_from_player()
+player_move(player_input, 'X')
 display_board()
